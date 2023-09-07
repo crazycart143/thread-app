ca23b49b76887dcdfa5b83547fa82543a07b56fa
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-09-07 23:40:33 +0800

    added constants and public assets

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,46 @@
+import {
+  OrganizationSwitcher,
+  SignOutButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" height={28} width={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          {/* THE CODE INSIDE SIGNED IN WILL ONLY SHOW IF USER IS SIGNED IN */}
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <UserButton />
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4 ",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 }
