f9a3bceeddabe2a5cb369eadcbc701d1faaa9102
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-09-11 17:06:04 +0800

    added uploadthing utils

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { z } from "zod";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -71,6 +73,14 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   function onSubmit(values: z.infer<typeof UserValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
+
+    if (hasImageChanged) {
+      const imgRes = useUploadThing();
+    }
     console.log(values);
   }
 
